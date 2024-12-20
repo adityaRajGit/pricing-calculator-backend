d7615978638726aebad2d2b33b0836a6c933a1a7
Aditya Raj <adityarajcv@outlook.com>
2024-12-21 00:06:23 +0530

    added Weight Handling Fee

--- services/feeCalculator.js
+++ services/feeCalculator.js
@@ -18,4 +18,25 @@ function calculateReferralFee(category, price, feeStructures) {
     }
   }
   
+  function calculateWeightHandlingFee(weight, shippingMode, serviceLevel, location, feeStructures) {
+    const fees = feeStructures.weightHandlingFees[shippingMode]?.[serviceLevel];
+    if (!fees) {
+      throw new Error('Invalid shipping mode or service level');
+    }
   
+    let totalFee = 0;
+    const weightInGrams = weight * 1000;
+  
+    if (weightInGrams <= 500) {
+      totalFee = fees.first500g[location];
+    } else if (weightInGrams <= 1000) {
+      totalFee = fees.first500g[location] + fees.additional500gUpTo1kg[location];
+    } else {
+      const additionalKgs = Math.ceil((weightInGrams - 1000) / 1000);
+      totalFee = fees.first500g[location] + 
+                 fees.additional500gUpTo1kg[location] + 
+                 (additionalKgs * fees.additionalKgAfter1kg[location]);
+    }
+  
+    return totalFee;
+  }
